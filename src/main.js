74ae10c54b10f18bad5ba11f578ef2ab9046dd46
quiquelhappy <quiquelhappy@gmail.com>
2020-01-30 12:53:45 +0100

    added pwa support

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import VueTyperPlugin from 'vue-typer'
 import store from './store'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(VueTyperPlugin)
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'gforum',
+    themeColor: '#272727',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
 }
